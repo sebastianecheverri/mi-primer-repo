d1b6ee1a914fea366ba3e1eae37ce911b28cac12
Sebastian <sebastianecheverri900@gmail.com>
2018-08-01 20:43:40 -0500

    ejercicios con http y sinatra

--- bundle_test/First_Sinatra_app/app.rb
+++ bundle_test/First_Sinatra_app/app.rb
@@ -6,8 +6,8 @@ get "/" do
 end
 
 get '/home' do
-    "Estoy en Home"
-    @saludo = "Welcome"
+    @greet = 'Estoy en home'
+    erb :home, layout: :layout_home
 end
 
 get '/contact' do
@@ -20,3 +20,34 @@ get '/productos' do
     "Estoy en productos"
     @productos = [1,2,3,4,5,6]
 end
+
+get '/greet' do
+    "Hola #{params[:name]}"
+end
+
+get '/age-in-5-years/:age' do
+    @age = params[:age].to_i
+    "Tu edad en 5 años será #{@age + 5}"
+end
+
+get '/' do
+    status 200
+    headers "Content-Type" => "text/html"
+    body "Hola Mundo"
+
+    erb :index
+end
+
+post '/make-a-post' do
+    [200, {"Content-Type" => "text/hmtl" }, "Acabas de hacer un post"]
+end
+
+put '/make-a-put' do
+    [200, {"Content-Type" => "text/hmtl" }, "Acabas de hacer un put"]
+end
+
+delete '/make-a-delete' do
+    [200, {"Content-Type" => "text/hmtl" }, "Acabas de hacer un delete"]
+end
+
+#HTTParty.put('http://localhost:4567/make-a-put', :headers => {"Content-Type" => 'application/json'})
